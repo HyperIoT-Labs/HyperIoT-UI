267c52cad5e7e2eb7196d421d40b0f423c35260e
Giona Martignani <giona.martignani@acsoftware.it>
2019-07-29 15:08:18 +0200

    Login form updated

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { AuthenticationService, JWTLoginResponse } from '@hyperiot/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthenticationHttpErrorHandlerService } from 'src/app/services/authentication-http-error-handler.service';
 import { Handler } from 'src/app/services/models/models';
+
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-login',
@@ -15,36 +17,98 @@ export class LoginComponent implements OnInit {
 
   error: string[] = [null, null, null];
 
-  loginForm = new FormGroup({
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required)
+  returnUrl: String;
+
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required),
+    rememberMe: new FormControl(false, Validators.required)
   });
+
+  cookieValue: String;
+
+  private key: string = "9$&hy7Ke2/";
+
+  private encrypted;
+
+  private decrypted;
+
+  loading: boolean = false;
 
   constructor(
     private authenticationService: AuthenticationService,
     private cookieService: CookieService,
     private router: Router,
+    private route: ActivatedRoute,
     private httperrorHandler: AuthenticationHttpErrorHandlerService
   ) { }
 
   ngOnInit() {
+    
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+    if (this.cookieService.check('hytUser')) {
+      this.decrypting(this.cookieService.get('hytUser'), this.key)
+      console.log(this.decrypted.toString(CryptoJS.enc.Utf8).split("&")[0])
+
+      this.loginForm = new FormGroup({
+        username: new FormControl(this.decrypted.toString(CryptoJS.enc.Utf8).split("&")[0], Validators.required),
+        password: new FormControl(this.decrypted.toString(CryptoJS.enc.Utf8).split("&")[1], Validators.required),
+        rememberMe: new FormControl(true, Validators.required)
+      });
+    } else {
+      this.loginForm = new FormGroup({
+        username: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required),
+        rememberMe: new FormControl(false, Validators.required)
+      });
+    }
+
+    console.log(this.loginForm)
   }
 
   login() {
+    this.loading= true;
+    console.log(this.loginForm);
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
     this.authenticationService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe(
       res => {
+        console.log("res")
         var jwtToken = <JWTLoginResponse>res;
         this.cookieService.set('HIT-AUTH', jwtToken.token, 2);
-        this.router.navigate(['/test']);
+        this.router.navigate([this.returnUrl]);
+
+        if (this.loginForm.value.rememberMe == true) {
+          this.encrypting(this.loginForm.value.username + "&" + this.loginForm.value.password, this.key);
+          this.cookieService.set('hytUser', this.encrypted, 28);
+          this.cookieValue = this.cookieService.get('hytUser');
+        } else if (this.cookieService.check('hytUser')) {
+          this.cookieService.delete('hytUser');
+        }
+        this.loading = false;
       },
       err => {
+        console.log("err")
         let k: Handler[] = this.httperrorHandler.handlePwdRecovery(err);
         for (let e of k) {
           this.error[2] = e.message;
         }
         console.log(this.error)
+        this.loading = false;
       }
     )
+  }
+
+  private encrypting(message: string, key: string) {
+    this.encrypted = CryptoJS.AES.encrypt(message, key);
+  }
+
+  private decrypting(encrypted: string, key: string) {
+    this.decrypted = CryptoJS.AES.decrypt(encrypted, key);
   }
 
 }
