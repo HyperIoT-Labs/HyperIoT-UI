cbf6b1141654455eac539afb5d1f4d90d5110a3e
Gabriele Losiczko <gabriele.losiczko@acsoftware.it>
2019-07-30 10:41:54 +0200

    Logout implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'hyt-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
 
-  constructor(private route: Router) { }
+  constructor(private route: Router, private cookieService: CookieService) { }
 
   showToolBar(): boolean {
     return (this.route.routerState.snapshot.root.children[0] != undefined) ? this.route.routerState.snapshot.root.children[0].data.showToolBar : true;
+  }
+
+  logout() {
+    this.cookieService.delete('HIT-AUTH');
+    this.route.navigate(['/authenitcation']);
   }
 
 }
--- src/app/configuration-modules/custom-material.module.ts
+++ src/app/configuration-modules/custom-material.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   exports: [
     CommonModule,
