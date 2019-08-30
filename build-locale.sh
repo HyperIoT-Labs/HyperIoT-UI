bf44e98e5581a8fe6d47d3a43abde8e0706d41d4
Gene <generoso.martello@acsoftware.it>
2019-08-30 12:19:56 +0200

    replaced i18n variables with 'HYT' prefix in dashboard and widgets files; added tool for merging translation files

---
+++ build-locale.sh
@@ -0,0 +1,11 @@
+./node_modules/xlf-merge/index.js \
+    src/locale/main/*.en-US.xlf \
+    src/locale/widgets/*.en-US.xlf \
+    src/locale/dashboard/*.en-US.xlf \
+    -o src/locale/translations.en-US.xlf
+
+./node_modules/xlf-merge/index.js \
+    src/locale/main/*.it-IT.xlf \
+    src/locale/widgets/*.it-IT.xlf \
+    src/locale/dashboard/*.it-IT.xlf \
+    -o src/locale/translations.it-IT.xlf
