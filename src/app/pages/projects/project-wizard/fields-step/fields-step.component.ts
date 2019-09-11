d3ebe1aa5c1cbd1edcd0d6abd12ed832b2e8fb67
Gabriele Losiczko <gabriele.losiczko@acsoftware.it>
2019-09-11 15:08:00 +0200

    fields treeview (first)

--- src/app/pages/projects/project-wizard/fields-step/fields-step.component.ts
+++ src/app/pages/projects/project-wizard/fields-step/fields-step.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, OnChanges } from '@angular/core';
 import { Option } from '@hyperiot/components/lib/hyt-radio-button/hyt-radio-button.component';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HPacketField, HpacketsService, HPacket, HDevice } from '@hyperiot/core';
 import { SelectOption } from '@hyperiot/components';
-import { HttpClient } from '@angular/common/http';
 import { HYTError } from 'src/app/services/errorHandler/models/models';
 import { ProjectWizardHttpErrorHandlerService } from 'src/app/services/errorHandler/project-wizard-http-error-handler.service';
+import { Node } from '@hyperiot/components/lib/hyt-tree-view-editable/hyt-tree-view-editable.component';
+import { Settings } from 'http2';
+
+interface PacketNode extends Node {
+  id: number;
+}
+
+interface PacketTreeView {
+  packetName: string;
+  tree: PacketNode[];
+}
 
 @Component({
   selector: 'hyt-fields-step',
   templateUrl: './fields-step.component.html',
   styleUrls: ['./fields-step.component.scss']
 })
-export class FieldsStepComponent implements OnInit {
+export class FieldsStepComponent implements OnInit, OnChanges {
 
   @Input() hDevices: HDevice[] = [];
 
   @Input() hPackets: HPacket[] = [];
+
+  packetTreeView: PacketTreeView[] = [];
 
   fieldForm: FormGroup;
 
@@ -51,19 +63,53 @@ export class FieldsStepComponent implements OnInit {
     private errorHandler: ProjectWizardHttpErrorHandlerService
   ) { }
 
-  ngOnInit() {
+  ngOnInit() { }
 
-    // for (let el of this.hDeviceList)
-    //   this.devicesOptions.push({ value: el.id.toString(), label: el.deviceName })
+  devicesOptions: SelectOption[] = [];
 
-    this.fieldForm = this.fb.group({});
+  currentDevice: HDevice;
+
+  ngOnChanges() {
+    this.devicesOptions = [];
+    this.packetTreeView = [];
+    for (let el of this.hDevices)
+      this.devicesOptions.push({ value: el.id.toString(), label: el.deviceName })
+    this.updatePacketTreeView();
   }
 
-  idPacket: number = 417;
-
-  sethPackets(id: number) {
-    this.idPacket = id;
+  updatePacketTreeView() {
+    this.packetTreeView = [];
+    if (this.currentDevice) {
+      let packetsForDevice = this.hPackets.filter(x => x.device.id == this.currentDevice.id)
+      packetsForDevice.forEach(x => {
+        this.packetTreeView.push({
+          packetName: x.name,
+          tree: this.createTree(x.fields)
+        })
+      })
+    }
   }
+
+  createTree(fields: HPacketField[]): PacketNode[] {
+    let node: PacketNode[] = [];
+    fields.forEach(element => {
+      node.push({
+        id: element.id,
+        name: element.name,
+        lom: element.multiplicity,
+        type: element.type,
+        children: (element.innerFields) ? this.createTree(element.innerFields) : null
+      })
+    });
+    return node;
+  }
+
+  deviceChanged(event) {
+    this.currentDevice = this.hDevices.find(x => x.id == event.value)
+    this.updatePacketTreeView();
+  }
+
+  idPacket: string;
 
   createField() {
 
@@ -74,13 +120,17 @@ export class FieldsStepComponent implements OnInit {
       name: this.fieldForm.value['fieldName'],
       multiplicity: this.fieldForm.value.fieldMultiplicity.value,
       type: this.fieldForm.value.fieldType,
-      description: this.fieldForm.value.fieldDescription
+      description: this.fieldForm.value.fieldDescription,
+      innerFields: (this.fieldForm.value.fieldMultiplicity.value == 'SINGLE') ? null : []
     }
 
-    this.hPacketService.addHPacketField(this.idPacket, hPacketField).subscribe(
+    let id = this.hPackets.find(x => x.name == this.idPacket).id;
+
+    this.hPacketService.addHPacketField(id, hPacketField).subscribe(//    this.hPacketService.addHPacketField(this.idPacket, hPacketField).subscribe(
       res => {
-        this.hPackets.find(x => x.id == this.idPacket).fields.push(res);
+        this.hPackets.find(x => x.id == id).fields.push(res);//  this.hPackets.find(x => x.id == this.idPacket).fields.push(res);
         this.hPacketsOutput.emit(this.hPackets);
+        this.fieldForm = null;
       },
       err => {
         this.errors = this.errorHandler.handleCreateField(err);
@@ -106,6 +156,16 @@ export class FieldsStepComponent implements OnInit {
       this.fieldForm.get('fieldName').invalid ||
       this.fieldForm.get('fieldType').invalid
     )
+  }
+
+  addField(event) {
+    console.log(event)
+    this.idPacket = event.name;//ID instead of NAME
+    this.fieldForm = this.fb.group({});
+  }
+
+  removeField() {
+    console.log("remove")
   }
 
 }
