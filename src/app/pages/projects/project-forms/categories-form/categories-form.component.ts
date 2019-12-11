0b7092d0b6ec67239e2c415d1fbeea50c2e4fae6
Gabriele Losiczko <gabriele.losiczko@acsoftware.it>
2019-12-11 18:11:58 +0100

    categories

--- src/app/pages/projects/project-forms/categories-form/categories-form.component.ts
+++ src/app/pages/projects/project-forms/categories-form/categories-form.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, Injector, ViewChild, ElementRef } from '@angular/core';
 import { ProjectFormEntity } from '../project-form-entity';
 import { I18n } from '@ngx-translate/i18n-polyfill';
-import { TreeNodeCategory } from '@hyperiot/components/public-api';
+import { TreeNodeCategory, CategoryTreeEvent } from '@hyperiot/components/public-api';
 import { AssetscategoriesService, AssetCategory } from '@hyperiot/core';
+import { DeleteConfirmDialogComponent } from 'src/app/components/dialogs/delete-confirm-dialog/delete-confirm-dialog.component';
+import { HytModalConfService } from 'src/app/services/hyt-modal-conf.service';
+import { Router } from '@angular/router';
+import { LoadStatus } from 'src/app/models/loadStatus';
 
 @Component({
   selector: 'hyt-categories-form',
@@ -10,6 +14,12 @@ import { AssetscategoriesService, AssetCategory } from '@hyperiot/core';
   styleUrls: ['./categories-form.component.scss']
 })
 export class CategoriesFormComponent extends ProjectFormEntity implements OnInit {
+
+  projectId: number;
+
+  categoryStatus: LoadStatus = LoadStatus.Default;
+
+  assetCategories: AssetCategory[] = [];
 
   categoriesFlatTree: TreeNodeCategory[] = [];
 
@@ -19,44 +29,99 @@ export class CategoriesFormComponent extends ProjectFormEntity implements OnInit
 
   constructor(
     injector: Injector,
-    private assetCategoriesService: AssetscategoriesService,
     @ViewChild('form', { static: true }) formView: ElementRef,
-    private i18n: I18n
+    private i18n: I18n,
+    private router: Router,
+    private assetCategoriesService: AssetscategoriesService,
+    private modalService: HytModalConfService
   ) {
     super(injector, i18n, formView);
-    this.longDefinition = 'Lorem ipsum...';
     this.formTitle = 'Project Categories';
-    this.icon = this.entitiesService.packet.icon;
+    this.hideDelete = true;
+    this.showSave = false;
+    this.projectId = +this.router.url.split('/')[2];
+  }
+
+  flatCategories() {
+    this.categoriesFlatTree = this.assetCategories.map((d) => ({
+      id: d.id,
+      label: d.name,
+      parent: null,
+      children: null,
+      data: d,
+      active: false
+    }));
+    this.categoriesFlatTree.forEach(x => {
+      x.parent = (x.data.parent) ? this.categoriesFlatTree.find(y => y.id === x.data.parent.id) : null;
+      // x.active = this.packet.categoryIds.includes(x.id);
+    });
+    this.categoriesFlatTree = [...this.categoriesFlatTree];
   }
 
   ngOnInit() {
     this.assetCategoriesService.findAllAssetCategory().subscribe(
       (res: AssetCategory[]) => {
-        res.forEach(x => {
-          this.categoriesFlatTree.push({
-            id: x.id,
-            label: x.name,
-            parent: null,
-            children: [],
-            data: x,
-            active: false
-          });
-        });
-        this.categoriesFlatTree.forEach(x => {
-          x.parent = (x.data.parent) ? this.categoriesFlatTree.find(y => y.id === x.data.parent.id) : null;
-          // x.active = this.packet.categoryIds.includes(x.id);
-        });
-        this.categoriesFlatTree = [...this.categoriesFlatTree];
+        this.assetCategories = [...res];
+        this.flatCategories();
+        this.categoryStatus = LoadStatus.Loaded;
+      },
+      err => {
+        this.categoryStatus = LoadStatus.Error;
       }
     );
   }
 
-  cbChange(event) {
-
+  treeAction(event: CategoryTreeEvent) {
+    switch (event.action) {
+      case 'add':
+        this.openCategoryModal((event.node) ? event.node.data : null, 'add');
+        break;
+      case 'delete':
+        this.openDelete(event.node.id);
+        break;
+      case 'edit':
+        this.openCategoryModal(event.node.data, 'edit');
+        break;
+    }
   }
 
-  cbAdd(event) {
+  categoryCreated(category: AssetCategory) {
+    if (this.assetCategories.some(t => t.id === category.id)) {
+      this.assetCategories.find(t => t.id === category.id).name = category.name;
+    } else {
+      this.assetCategories.push(category);
+    }
+    this.flatCategories();
+  }
 
+  openCategoryModal(category: AssetCategory, mode: string) {
+    this.modalService.open('hyt-add-cetegory-modal', { projectId: this.projectId, category, mode });
+  }
+
+  removeCategoryAndChildren(categoryId: number) {
+    this.assetCategories.filter(t => (t.parent) ? t.parent.id === categoryId : false).forEach(element => {
+      this.removeCategoryAndChildren(element.id);
+    });
+    this.assetCategories = this.assetCategories.filter(t => t.id !== categoryId);
+  }
+
+  private openDelete(id: number) {
+    const dialogRef = this.dialog.open(DeleteConfirmDialogComponent, {
+      data: { title: 'Delete item?', message: 'This operation cannot be undone.' }
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'delete') {
+        this.assetCategoriesService.deleteAssetCategory(id).subscribe(
+          res => {
+            this.removeCategoryAndChildren(id);
+            this.flatCategories();
+          },
+          err => {
+            //TODO handle error
+          }
+        );
+      }
+    });
   }
 
 }
--- src/app/pages/projects/project-forms/packet-enrichment-form/asset-category/asset-category.component.ts
+++ src/app/pages/projects/project-forms/packet-enrichment-form/asset-category/asset-category.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AssetscategoriesService, AssetCategory, HPacket, HProject } from '@hyperiot/core';
-import { TreeNodeCategory } from '@hyperiot/components';
+import { TreeNodeCategory, CategoryTreeEvent } from '@hyperiot/components';
+import { LoadStatus } from 'src/app/models/loadStatus';
+import { HytModalConfService } from 'src/app/services/hyt-modal-conf.service';
+import { DeleteConfirmDialogComponent } from 'src/app/components/dialogs/delete-confirm-dialog/delete-confirm-dialog.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'hyt-asset-category',
@@ -13,6 +17,10 @@ export class AssetCategoryComponent implements OnInit {
 
   @Input() project: HProject;
 
+  assetCategories: AssetCategory[] = [];
+
+  categoryStatus: LoadStatus = LoadStatus.Default;
+
   @Output() categoryIds: EventEmitter<number[]> = new EventEmitter<number[]>();
 
   categoriesFlatTree: TreeNodeCategory[] = [];
@@ -20,70 +28,106 @@ export class AssetCategoryComponent implements OnInit {
   catIds: number[] = [];
 
   constructor(
-    private assetCategoriesService: AssetscategoriesService
+    private assetCategoriesService: AssetscategoriesService,
+    private dialog: MatDialog,
+    private modalService: HytModalConfService
   ) { }
 
   ngOnInit() {
     this.assetCategoriesService.findAllAssetCategory().subscribe(
       (res: AssetCategory[]) => {
-        res.forEach(x => {
-          this.categoriesFlatTree.push({
-            id: x.id,
-            label: x.name,
-            parent: null,
-            children: [],
-            data: x,
-            active: false
-          });
-        });
-        this.categoriesFlatTree.forEach(x => {
-          x.parent = (x.data.parent) ? this.categoriesFlatTree.find(y => y.id === x.data.parent.id) : null;
-          // x.active = this.packet.categoryIds.includes(x.id);
-        });
-        this.categoriesFlatTree = [...this.categoriesFlatTree];
+        this.assetCategories = [...res];
+        this.flatCategories();
+        this.categoryStatus = LoadStatus.Loaded;
+      },
+      err => {
+        this.categoryStatus = LoadStatus.Error;
       }
     );
   }
 
-  fillCatIds() {
-    this.catIds = [];
+  flatCategories() {
+    this.categoriesFlatTree = this.assetCategories.map((d) => ({
+      id: d.id,
+      label: d.name,
+      parent: null,
+      children: null,
+      data: d,
+      active: this.catIds.some(n => n === d.id)
+    }));
     this.categoriesFlatTree.forEach(x => {
-      if (x.children.length === 0 && x.active) {
-        this.catIds.push(x.id);
-      }
+      x.parent = (x.data.parent) ? this.categoriesFlatTree.find(y => y.id === x.data.parent.id) : null;
+      // x.active = this.packet.categoryIds.includes(x.id);
     });
+    this.categoriesFlatTree = [...this.categoriesFlatTree];
   }
 
-  cbChange(event) {
+  fillCatIds() {
+    this.catIds = this.categoriesFlatTree
+      .filter(x => x.children.length === 0 && x.active)
+      .map(x => x.id);
+    console.log(this.catIds);
+  }
+
+  treeAction(event: CategoryTreeEvent) {
+    switch (event.action) {
+      case 'add':
+        this.openCategoryModal((event.node) ? event.node.data : null, 'add');
+        break;
+      case 'delete':
+        this.openDelete(event.node.id);
+        break;
+      case 'checked':
+        this.cbChange();
+        break;
+      case 'edit':
+        this.openCategoryModal(event.node.data, 'edit');
+        break;
+    }
+  }
+
+  openCategoryModal(category: AssetCategory, mode: string) {
+    this.modalService.open('hyt-add-cetegory-modal', { projectId: this.project.id, category, mode });
+  }
+
+  cbChange() {
     this.fillCatIds();
     this.categoryIds.emit(this.catIds);
   }
 
-  cbAdd(event) {
-    const data: AssetCategory = {
-      entityVersion: 1,
-      name: event.label,
-      owner: {
-        ownerResourceName: 'it.acsoftware.hyperiot.hproject',
-        ownerResourceId: this.project.id
-      },
-      parent: event.parent ? event.parent.data : null
-    };
+  categoryCreated(category: AssetCategory) {
+    if (this.assetCategories.some(t => t.id === category.id)) {
+      this.assetCategories.find(t => t.id === category.id).name = category.name;
+    } else {
+      this.assetCategories.push(category);
+    }
+    this.flatCategories();
+  }
 
-    this.assetCategoriesService.saveAssetCategory(data).subscribe(
-      (res: AssetCategory) => {
-        this.categoriesFlatTree.push({
-          id: res.id,
-          label: res.name,
-          parent: event.parent,
-          children: [],
-          data: res,
-          active: false
-        });
-        this.categoriesFlatTree = [...this.categoriesFlatTree];
-      },
-      err => console.log(err)
-    );
+  removeCategoryAndChildren(categoryId: number) {
+    this.assetCategories.filter(t => (t.parent) ? t.parent.id === categoryId : false).forEach(element => {
+      this.removeCategoryAndChildren(element.id);
+    });
+    this.assetCategories = this.assetCategories.filter(t => t.id !== categoryId);
+  }
+
+  private openDelete(id: number) {
+    const dialogRef = this.dialog.open(DeleteConfirmDialogComponent, {
+      data: { title: 'Delete item?', message: 'This operation cannot be undone.' }
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'delete') {
+        this.assetCategoriesService.deleteAssetCategory(id).subscribe(
+          res => {
+            this.removeCategoryAndChildren(id);
+            this.flatCategories();
+          },
+          err => {
+            //TODO handle error
+          }
+        );
+      }
+    });
   }
 
 }
