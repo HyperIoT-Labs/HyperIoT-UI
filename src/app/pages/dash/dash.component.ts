f1d44188214f68fe3f20c5f079e278d742386de6
Marco Brunet <marco.brunet@acsoftware.it>
2022-05-24 11:31:16 +0200

    Add dash logic for project selection

--- src/app/pages/dash/dash.component.ts
+++ src/app/pages/dash/dash.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CookieService} from 'ngx-cookie-service';
+import {Area, AreasService} from '@hyperiot/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'hyt-dash',
@@ -8,14 +10,36 @@ import {CookieService} from 'ngx-cookie-service';
 })
 export class DashComponent implements OnInit {
   idProjectSelected = 0;
+  showAreas = false;
+  areaId = 0
+  areaPath: any[] = [];
 
-  constructor(private cookieService: CookieService) { }
+  constructor(
+    private cookieService: CookieService,
+    private areaService: AreasService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
-    const user = JSON.parse(this.cookieService.get('user'));
+/*    const user = JSON.parse(this.cookieService.get('user'));
     this.idProjectSelected = user?.id === 502 ? 503 : 425;
-    console.log(user);
+    console.log(user);*/
     // this.idProjectSelected = 425;
+
+    this.showAreas = this.activatedRoute.snapshot.routeConfig.path.startsWith('areas/');
+
+    if (this.showAreas) {
+      // load area realtime Dashboard
+      this.areaId = +this.activatedRoute.snapshot.params.areaId;
+      this.idProjectSelected = +this.activatedRoute.snapshot.params.projectId;
+      if (this.areaId) {
+        this.areaService.getAreaPath(this.areaId).subscribe((areas: Area[]) => {
+          this.areaPath = areas;
+        });
+      }
+    }
+
   }
 
 }
