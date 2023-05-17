c80e756a569a7b46d218cb0216ea311034be583f
Gabriele Losiczko <gabriele.losiczko@gmail.com>
2023-05-17 18:07:28 +0200

    widget settings nested fields sort, treeview expansion to selected

--- projects/components/src/lib/hyt-inner-field-select/hyt-inner-field-select.component.ts
+++ projects/components/src/lib/hyt-inner-field-select/hyt-inner-field-select.component.ts
@@ -25,8 +25,56 @@ export class HytInnerFieldSelectComponent implements OnChanges, OnInit {
     private hPacketFieldsHandlerService: HPacketFieldsHandlerService
   ) { }
 
+  findTreePathToField(fields: HPacketField[], targetField: HPacketField, path: HPacketField[] = []): HPacketField[] | null {
+    for (const field of fields) {
+      if (field === targetField) {
+        return [...path, field];
+      }
+      if (field.innerFields) {
+        const foundPath = this.findTreePathToField(field.innerFields, targetField, [...path, field]);
+        if (foundPath) {
+          return foundPath;
+        }
+      }
+    }
+    return null;
+  }
+
+  expandTreePathToField(field: HPacketField) {
+    const treePath: HPacketField[] | null = this.findTreePathToField(this.dataSource.data, field);
+    if (treePath) {
+      treePath.forEach(field => this.treeControl.expand(field));
+    }
+  }
+
+  sortFields(fields: HPacketField[]): HPacketField[] {
+    return fields
+      .sort((a, b) => {
+        // Sort fields with children above fields without children
+        if (a.innerFields.length && !b.innerFields.length) {
+          return -1;
+        } else if (!a.innerFields.length && b.innerFields.length) {
+          return 1;
+        }
+        // Sort fields alphabetically by name
+        return a.name.localeCompare(b.name)
+      })
+      .map(field => {
+        if (field.innerFields) {
+          field.innerFields = this.sortFields(field.innerFields);
+        }
+        return field;
+      });
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
-    this.dataSource.data = this.fields;
+    const sortedFields = this.sortFields(this.fields)
+    this.dataSource.data = sortedFields;
+    if (this.selectedFieldsIds.length > 0) {
+      this.selectedFieldsIds.forEach(fieldId => {
+        this.expandTreePathToField(this.hPacketFieldsHandlerService.findFieldFromFieldsTree(this.fields, fieldId));
+      });
+    }
     const fieldsFlatList = this.hPacketFieldsHandlerService.flatFieldsTree(this.fields);
     this.fieldsOption = fieldsFlatList.map(x => ({
       value: x.field.id,
