34596e75d2927f7fb9ef2001feaa890937f143b2
VanHiro <maccherinivanessa04@gmail.com>
2024-02-14 12:44:33 +0100

    subscribe datachannel and show field's alias

--- projects/widgets/src/lib/widget/dynamic-label-value-widget/dynamic-label-value-widget.component.ts
+++ projects/widgets/src/lib/widget/dynamic-label-value-widget/dynamic-label-value-widget.component.ts
@@ -1,5 +1,6 @@
 import { Component, Injector, OnInit } from '@angular/core';
-import { Logger, LoggerService } from 'core';
+import { DataChannel, DataPacketFilter, Logger, LoggerService } from 'core';
+import { PartialObserver, Subject } from 'rxjs';
 import { BaseWidgetComponent } from '../../base/base-widget/base-widget.component';
 
 @Component({
@@ -7,35 +8,36 @@ import { BaseWidgetComponent } from '../../base/base-widget/base-widget.componen
   templateUrl: './dynamic-label-value-widget.component.html',
   styleUrls: ['../../../../../../src/assets/widgets/styles/widget-commons.css', './dynamic-label-value-widget.component.scss']
 })
-export class DynamicLabelValueWidgetComponent
-  extends BaseWidgetComponent
-  implements OnInit {
+export class DynamicLabelValueWidgetComponent extends BaseWidgetComponent implements OnInit {
+  /**
+   * DataChannel to subscribe to retrieve data
+   */
+  dataChannel: DataChannel;
 
-  labels: Array<string> = ['Versione', 'Operatore', 'Ordine', 'Materia Prima', 'Sacchi prodotti', 'Sacchi residui'];
-  values: Array<any> = ['1.2.3', 'Massimo', 21332, 'Plastica', 20, 100];
+  chartLabels: any = [];
+  chartData: any = [];
 
   loadingOfflineData: boolean = false;
 
   protected logger: Logger;
+
+  allData$: Subject<any[]> = new Subject();
 
   constructor(
     injector: Injector,
     protected loggerService: LoggerService
   ) {
     super(injector, loggerService);
-    debugger
     this.logger = new Logger(this.loggerService);
     this.logger.registerClass(DynamicLabelValueWidgetComponent.name);
   }
 
   ngOnInit(): void {
-    debugger
     super.ngOnInit();
     this.configure();
   }
 
   configure() {
-    debugger
     if (
       !(
         this.widget.config != null &&
@@ -48,9 +50,57 @@ export class DynamicLabelValueWidgetComponent
       return;
     }
     this.isConfigured = true;
+    const labelsIds = Object.keys(this.widget.config.packetFields);
+    labelsIds.forEach(id => this.chartLabels.push(this.widget.config.fieldAliases[id] ?
+      { id: this.widget.config.packetFields[id], label: this.widget.config.fieldAliases[id] } :
+      { id: this.widget.config.packetFields[id], label: this.widget.config.packetFields[id] }));
+
+
+    this.initStream();
+    const dataPacketFilter = new DataPacketFilter(
+      this.widget.config.packetId,
+      this.widget.config.packetFields,
+      true
+    );
+    this.subscribeRealTimeStream(dataPacketFilter, (eventData) => {
+      this.allData$.next(eventData);
+    });
   }
 
-  isString(value: string | number): boolean {
-    return typeof value === 'string';
+  subscribeRealTimeStream(dataPacketFilter: DataPacketFilter, observerCallback: PartialObserver<[any, any]> | any): void {
+    this.dataChannel = this.dataService.addDataChannel(+this.widget.id, [dataPacketFilter]);
+    this.dataSubscription = this.dataChannel.subject.subscribe(observerCallback);
+  }
+
+  /**
+   * Manipulate stream data from allData$ and set observer for pause/play features
+   */
+  initStream() {
+    if (this.initData.length > 0) {
+      this.convertAndBufferData(this.initData);
+    }
+    // subscribe data stream and update datatable
+    this.allData$.pipe().subscribe((packet) => {
+      if (packet['data'].length > 0) {
+        this.convertAndBufferData([packet]);
+      } else {
+        this.logger.debug('initStream: data is empty');
+      }
+    });
+  }
+
+  convertAndBufferData(packet) {
+    packet.forEach((packetData) => {
+      if (packetData.length === 0) {
+        return;
+      }
+      packetData.data.forEach(element => {
+        Object.values(this.widget.config.packetFields).forEach(key => {
+          if (element.hasOwnProperty(key)) {
+            this.chartData[key] = element[key];
+          }
+        });
+      });
+    })
   }
 }
