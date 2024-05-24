a17c3eecb10c086f8243e0f007a1dfff32a059e2
Giovanni Colacitti <giovannicolacitti@gmail.com>
2024-05-24 15:56:36 +0200

    Sub area navigation Dashboard

--- src/app/pages/areas/container-area-map/container-area-map.component.ts
+++ src/app/pages/areas/container-area-map/container-area-map.component.ts
@@ -224,6 +224,13 @@ export class ContainerAreaMapComponent implements OnInit, OnDestroy {
   onTreeNodeClick(node) {
     this.logger.debug('onTreeNodeClick start', node);
     this.loadArea(node.data.item);
+    if(node.data.item.id !== this.areaId){
+      this.router.navigate(['areas', this.projectId, node.data.item.id])
+        .then(() => {
+          //TODO: Change method to navigate
+          this.loadArea(node.data.item);
+        })
+    }
   }
 
   /**
@@ -334,6 +341,11 @@ export class ContainerAreaMapComponent implements OnInit, OnDestroy {
     this.logger.debug('onItemMapClicked start', itemMap);
     if(itemMap.innerArea){
       this.loadArea(itemMap);
+      this.router.navigate(['areas', this.projectId, itemMap.id])
+        .then(() => {
+          //TODO: Change method to navigate
+          this.loadArea(itemMap);
+        })
     }
   }
 
